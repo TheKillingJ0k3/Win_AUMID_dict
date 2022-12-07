66b7c9da96bd3717bb34baaf832751c47ed8ea82
Th3KillingJ0k3 <ggpatoulis@hotmail.com>
2022-12-07 20:27:37 +0200

    kind of works but mixes up some values with keys

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Win_AUMID_dict
 Script that finds the Application User Model ID or AUMID of an installed app in Windows 10 and creates a dictionary via Excel with name of application as key and AUMID as value
+
+Note:
+Before using this, you should increase character limit per line in Windows Powershell, otherwise most AUMIDs will be incomplete. Here's how to do it:
+
+Open Windows PowerShell.
+Right click the top bar > Properties > Layout > Screen Buffer Size > Wrap text output on resize: uncheck + increase Width value, e.g. to 1200
+
+
+Works kind of ok, but mixes up some values at the moment.
--- main.py
+++ main.py
@@ -35,5 +35,31 @@ wb, ws = open_excel('.\\AUMID Excel\AUMID Excel.xlsx')
 bla = subprocess.run('"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" get-StartApps',capture_output=True, text=True, shell=True)
 print(bla)
 print(bla.stdout)
-ws.cell(row=1, column=1).value = bla.stdout
+test = (bla.stdout).split('  ')
+print(test)
+test = list(filter(lambda a: a != '', test))
+print(test)
+# for i in test:
+#     for row in range(0, len(test)/2):
+#     if i.startswith('\n'):
+#         ws.cell(row=row, column=1).value = i
+#     else:
+#         ws.cell(row=1, column=2).value = i
+        
+name = []
+appID = []
+for i in test:
+    if test.index(i)%2 == 0:
+        name.append(i)
+    else:
+        appID.append(i)
+print(name)
+print(appID)
+
+for i in name:
+    ws.cell(row=name.index(i)+1, column=1).value = i
+for i in appID:
+    ws.cell(row=appID.index(i)+1, column=2).value = i
+for i in range(1, ws.max_row+1):
+    ws.cell(row=i, column=3).value = '=CONCAT(CHAR(34),A1,CHAR(34),CHAR(58)," ",CHAR(34),B1,CHAR(34),CHAR(44))'# .format(ws.cell(row=i, column=1).value, ws.cell(row=i, column=2).value)
 wb.save('.\\AUMID Excel\AUMID Excel.xlsx')
